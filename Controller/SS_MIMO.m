e77c4fe80eefebd3d8fd835e0fed5134f3db667a
Qrace <54846053+Qrace@users.noreply.github.com>
2021-11-18 15:38:49 +0100

    Complient control with new polssss and no zeros

--- Controller/SS_MIMO.m
+++ Controller/SS_MIMO.m
@@ -8,27 +8,32 @@ C = eye(8,8);%zeros(4,8); C(1,1) = 1; C(2,2) = 1; C(3,3) = 1; C(4,4) = 1;
 sys_mimo=ss(A,B,C,0);
 
 %% Disigning feedback and observer gain
-Desired_poles = [-2:1/7:-1];
-Desired_poles_obs = [-2:1/7:-1];
+Xpole = roots([1.28 2*sqrt(251*1.28) 251]);
+Ypole = roots([1.28 2*sqrt(250*1.28) 250]);
+Zpole = roots([1.28 2*sqrt(250*1.28) 250]);
+Phipole = roots([1/2180 2*sqrt(251*(1/2180)) 251]);
 
-F_mimo=-place(A,B,Desired_poles); % State feedback
+Desired_poles = real([Xpole' Zpole' Ypole' Phipole']);
+Desired_poles_obs = 5*Desired_poles;
+
+F_mimo=-place(A,B,Desired_poles) % State feedback
 L_mimo=-place(A',C',Desired_poles_obs)'; % Observer
 
-%% Find N and M
-Desired_zeroes = [-2:1/7:-1];
-
-L = L_mimo;
-F = F_mimo;
-Aza = A+B*F+L*C;
-Cza = -F;
-
-M_tilde=-place(Aza',Cza',Desired_zeroes)';
-
-Acl = [A B*F;-L*C Aza];
-B_tilde_cl = [B;M_tilde];
-Ccl = [C zeros(8,8)];
-N = -pinv(Ccl*(Acl^-1)*B_tilde_cl);
-
-M = M_tilde*N;
-
+% %% Find N and M
+% Desired_zeroes = [-2:1/7:-1];
+% 
+% L = L_mimo;
+% F = F_mimo
+% Aza = A+B*F+L*C;
+% Cza = -F;
+% 
+% %M_tilde=-place(Aza',Cza',Desired_zeroes)';
+% 
+% Acl = [A B*F;-L*C Aza];
+% B_tilde_cl = [B;M_tilde];
+% Ccl = [C zeros(8,8)];
+% N = -pinv(Ccl*(Acl^-1)*B_tilde_cl);
+% 
+% M = M_tilde*N;
+% 
 
