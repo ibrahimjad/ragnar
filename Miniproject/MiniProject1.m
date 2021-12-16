35e0a47423842558ae161e6eda67de1f23040377
KristofferTheMachine <45390354+KristofferTheMachine@users.noreply.github.com>
2021-12-16 15:53:02 +0100

    abe

--- Miniproject/MiniProject1.m
+++ Miniproject/MiniProject1.m
@@ -6,22 +6,37 @@ close all
 f_wheels = rtccurve([0 0 0],0,10,20*4)
 % Arrival flow for 4 wheels
 f_ESP = rtccurve([0 0 0],0,10,8)
+f_network = rtccurve([[0 0 1000/8]])
+f_MM = rtccurve([0 0 0],0,40,1400)
+f_RC = rtccurve([0 0 0],0,40,1400)
+f_combined = rtcplus(rtccurve([0 0 0],0,40,1400+1400),rtccurve([0 0 0],0,10,(20*4*4)+(8*4)))
 
 figure(1)
-rtcplot(f_wheels,f_ESP,100)
-xlabel('t [Ms]')
-ylabel('k*v_T [B]')
+rtcplot(f_network,f_combined,200);%ylim([0 6000]);
+xlabel('Time [ms]')
+ylabel(' Bytes [Bytes]')
 title('Staircase arrival curves')
-legend('4 * wheels','ESP')
+legend('Network','Mean network arrival')
 
-%% 2.4
+%% 2.3
 %possion prob
- x=[15:65];
- y = poisspdf(x,40);
+%  x=[15:65];
+%  y = poisspdf(x,40);
+% figure(2)
+% bar(x,y,1)
+% xlabel('Time beteen packets [ms]')
+% ylabel('Probability')
+
+%% 2.4 rtccurve([[0 startY 0];[skiftvedtid skift_til 0]], 0, skift_igen, skiftilstarty+værdi);
+f_tokenbucketoutput1 = rtccurve([[0 10 105];[40 1400*3 35]]);
+f_tokenbucketoutput2 = rtccurve([[0 0 105];[40 35 0]], 0, 40, 4000);
 figure(2)
-bar(x,y,1)
-xlabel('Time beteen packets [ms]')
-ylabel('Probability')
+rtcplot(f_network,f_tokenbucketoutput1,f_tokenbucketoutput2,100)
+grid on
+xlabel('Time [ms]')
+ylabel(' Bytes [Bytes]')
+title('Token bucket output')
+legend('Network','Output of one token bucket')
 
 %%
 % 2.5 token bucket arrivel curves 
